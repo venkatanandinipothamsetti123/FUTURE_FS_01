afa0e74a8692549902e4e6ae997db535cd2b2f67
Venkata Nandini Pothamsetti <pothamsettivenkatanandini@gmail.com>
2026-06-13 18:57:22 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+let profile=document.getElementById("profile");
+profile.onclick=function(){
+    if(profile.src.includes("images/image_cf6f97a8.png")){
+        profile.src="images/file_000000008b8872098b67e40f8f9afbe1.png";}
+    else{
+        profile.src="images/image_cf6f97a8.png";
+    }
+
+    
+}
